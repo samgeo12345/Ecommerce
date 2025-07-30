6886fcfd1e7478f0a2642efe297a410604e2b08f
samgeo12345 <samgeoj12d@gmail.com>
2025-07-30 22:09:57 +0530

    Resta.

--- web-app/src/order.jsx
+++ web-app/src/order.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {useLocation} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar ,faSpinner} from '@fortawesome/free-solid-svg-icons';
 import { useParams } from "react-router-dom";
 import './order.css'
+import RecipeItems from "./seperateitem";
 
 function OrderItems() {
   const [Orders, setOrders]= useState(null);
@@ -25,15 +26,24 @@ function OrderItems() {
   };
 
   return (
-    <div className="maincontentdiv2">
-        <div className="contentdiv2" key={Orders.id}>
-            <div className="imgdiv2">
+    <div className="maincontentdiv3">
+        <div className="contentdiv3" key={Orders.id}>
+            <div className="imgdiv3">
             <img src={Orders.image} alt="" />
+            <div className="quantselectdiv">
+              <h3>{Orders.name}</h3>
+              <p>Quantity: {quantity}</p>
             </div>
-            <div className="itemdetaildiv2">
-            <h3>{Orders.name}</h3>
-            <p>{quantity}</p>
-            <button className="button">₹ {(Orders.caloriesPerServing-1)*quantity}</button>
+            </div>
+            <div className="enterdetaildiv">
+            <h1>Enter Details</h1>
+            <input type="text" placeholder="Name"/>
+            <input type="number" placeholder="Phone no"/>
+            <textarea type="text" placeholder="Address"></textarea>
+            <div className="cancelorderdiv">
+            <Link to="/" className="cancelbtn">Cancel</Link>
+            <Link className="rupeebtn">₹ {(Orders.caloriesPerServing-1)*quantity}</Link>
+            </div>
             </div>
         </div>
     </div>

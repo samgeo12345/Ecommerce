10724ac536dafd6d8746432ffde98f02dd41b200
samgeo12345 <samgeoj12d@gmail.com>
2025-08-03 16:48:52 +0530

    Resta.

--- web-app/src/purchased.jsx
+++ web-app/src/purchased.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import {useLocation} from 'react-router-dom'
+import {Link,useLocation} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar ,faSpinner, faCircleCheck} from '@fortawesome/free-solid-svg-icons';
+import { faStar ,faSpinner,faCircleChevronLeft, faCircleCheck} from '@fortawesome/free-solid-svg-icons';
 import './purchased.css'
 
 function Purchased() {
-    const purchase=useLocation();
-    const names=purchase.state.name;
-    const phone=purchase.state.number;
-    const address=purchase.state.place;
   return (
     <div className='wholepurchaseddiv'>
+        <Link to="/" className='tohome'><FontAwesomeIcon icon={faCircleChevronLeft} /> Home</Link>
         <div className="purchaseddiv">
             <h1>Payment received successfully!</h1>
             <FontAwesomeIcon className='i' icon={faCircleCheck}/>
